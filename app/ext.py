9e193db505d960936aedc187fb584e5f0487798b
Wesley Xie <wesley.w.xie@gmail.com>
2025-10-07 23:15:21 +0800

    chore: clean up

--- app/ext.py
+++ app/ext.py
@@ -1,8 +1,3 @@
 from app.config import Config
-from app.services.water_crawler import WaterCrawler
 
 CONFIG = Config()
-
-CRAWLER = WaterCrawler(
-    base_url=CONFIG.WATER_CRAWL_BASE_URL, api_key=CONFIG.WATER_CRAWL_API_KEY
-)
--- main.py
+++ main.py
@@ -1,49 +1,13 @@
-import json
 import logging
 
-from app.ext import CONFIG, CRAWLER
-from app.services.data_extractor import DataExtractor
+from app.ext import CONFIG
 
 logging.basicConfig(format=CONFIG.LOGGING_FORMAT, level=CONFIG.LOGGING_LEVEL)
 logger = logging.getLogger(__name__)
 
 
 def main():
-    url = "https://javdb.com/censored?page=1"
-    result = CRAWLER.scrape(
-        url=url,
-        page_options={
-            "include_tags": [".movie-list"],
-            "include_html": True,
-            "only_main_content": True,
-            "include_links": False,
-        },
-    )
-
-    html_content = result.get("result").get("html")
-    extractor = DataExtractor()
-    movie_list = extractor.extract_movie_list_item(
-        base_url=url, movie_data=html_content
-    )
-
-    logger.info(f"Found {len(movie_list)} movies, processing...")
-
-    for movie_list_item in movie_list:
-        html_content = (
-            CRAWLER.scrape(
-                url=movie_list_item.link,
-                page_options={
-                    "include_tags": [".video-detail"],
-                    "include_html": True,
-                    "only_main_content": True,
-                    "include_links": False,
-                },
-            )
-            .get("result")
-            .get("html")
-        )
-        movie_details = extractor.extract_movie_details(html_content)
-        logger.info(json.dumps(movie_details, indent=4))
+    logger.info("Starting...")
 
 
 if __name__ == "__main__":
